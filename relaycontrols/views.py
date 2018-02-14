9ce8b50c3dd6c65374602ad5a8df938c46ecf089
anon <prorevtion@gmail.com>
2018-02-14 04:04:08 -0600

    third commi

--- relaycontrols/views.py
+++ relaycontrols/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Relay
 
-# Create your views here.
+def relay_list(request):
+    relays = Relay.objects.all()
+    return render(request, 'relaycontrols/relay_list.html', {'relays': relays})
